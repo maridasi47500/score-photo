28dff2cc8dd0e750615728e48d1aa26cfca6906c
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2025-11-15 23:37:10 -0300

    qsfg

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   get "welcome/index"
   resources :lefthandtechniques
   resources :bowingtechniques
